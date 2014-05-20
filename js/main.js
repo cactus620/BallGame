418cb8ede7fd52f5324c571b895134c419b4cf08
cactus620 <cactus620@gmail.com>
2014-05-20 20:17:38 +0800

    add bg image

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 var gamejs = require('gamejs');
 
 function main() {
-    var display = gamejs.display.setMode([600, 400]);
+    var display = gamejs.display.setMode([400, 400]);
 
     var ball = gamejs.image.load('images/ball.png');
-    var unitPosition = [20, 20];
+    var bg = gamejs.image.load('images/bg.png');
+    var bgPosition = [0, 0];
+    var unitPosition = [90, 90];
  
     gamejs.onEvent(function() {
         // event handling
@@ -13,12 +15,15 @@ function main() {
     gamejs.onTick(function() {
         // game loop
       display.clear();
+      display.blit(bg, bgPosition);
       display.blit(ball, unitPosition);
+      
     });
 }
 
 gamejs.preload([
    'images/ball.png',
+   'images/bg.png'
 ]);
 
 gamejs.ready(main);
